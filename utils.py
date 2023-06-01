a2470af078bfec9ddd233a3cdcc7c10c38ea20af
srikanthp1 <psrikanth1409@gmail.com>
2023-06-01 23:35:18 +0530

    made utils changes

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 
+import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 def get_train_test_transforms():
@@ -99,4 +100,13 @@ def test(model, device, test_loader, criterion):
     
     return test_acc,test_losses
 
-# , reduction='sum'
+def plot_acc_loss(train_losses,test_losses,train_acc,test_acc):
+    fig, axs = plt.subplots(2,2,figsize=(15,10))
+    axs[0, 0].plot(train_losses)
+    axs[0, 0].set_title("Training Loss")
+    axs[1, 0].plot(train_acc)
+    axs[1, 0].set_title("Training Accuracy")
+    axs[0, 1].plot(test_losses)
+    axs[0, 1].set_title("Test Loss")
+    axs[1, 1].plot(test_acc)
+    axs[1, 1].set_title("Test Accuracy")
